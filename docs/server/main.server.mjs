4058ff480ef2ac605489575f3c03c7572c0c753e
Santha Ram <santharam243@gmail.com>
2025-06-10 22:31:51 +0530

    initial ui commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DNGSHL4O.mjs";import"./chunk-TJ6QMPX7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-5ZJGSPHE.mjs";import"./chunk-TJ6QMPX7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
